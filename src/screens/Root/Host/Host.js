48beeae7ffa345980a1b19ef8d3a66226de7d41f
Ryan Trainor <rptrainor@gmail.com>
2020-05-03 18:06:33 -0700

    styling the Host.js page'

--- src/screens/Root/Host/Host.js
+++ src/screens/Root/Host/Host.js
@@ -1,59 +1,82 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker'
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 export default function Host() {
-
-    const [selectedImage, setSelectedImage] = React.useState(null);
-
-    let openImagePickerAsync = async () => {
-      let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-  
-      if (permissionResult.granted === false) {
-        alert('Permission to access camera roll is required!');
-        return;
-      }
-  
-      let pickerResult = await ImagePicker.launchImageLibraryAsync();
-  
-      if (pickerResult.cancelled === true) {
-        return;
-      }
-  
-      setSelectedImage({ localUri: pickerResult.uri });
-    };
-  
-    if (selectedImage !== null) {
-      return (
-        <View style={styles.container}>
-          <Image
-            source={{ uri: selectedImage.localUri }}
-            style={styles.thumbnail}
-          />
-        </View>
-      );
-    }
-
   return (
     <View style={styles.container}>
-      <Text>Host.js</Text>
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-     </View>
+      <View style={styles.buttonGroup}>
+        <Ionicons name="ios-close" size={40} color="#005200" />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>QUERY</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.headerText}>What did you discover?</Text>
+      <TextInput style={styles.textInput} multiline={true} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      thumbnail: {
-        width: 300,
-        height: 300,
-        resizeMode: "contain"
-      }
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F2EEE4",
+    borderRadius: 10,
+    marginTop: 50,
+    alignItems: "stretch",
+    justifyContent: "center"
+  },
+  buttonGroup: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
+  },
+  headerText: {
+    flex: 1,
+    fontFamily: "Montserrat-Semibold",
+    fontSize: 20,
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    marginHorizontal: 5,
+    marginVertical: 8
+  },
+  textInput: {
+    flex: 8,
+    alignSelf: "center",
+    width: "90%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    fontSize: 16,
+    textAlignVertical: "top",
+    textAlign: "left",
+    fontFamily: "Lato"
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    alignItems: "center",
+    backgroundColor: "#9842BB",
+    borderColor: "#9842BB",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    fontFamily: "Montserrat-Semibold",
+  },
+});
